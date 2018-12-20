a9e4f160234662e2592e2a33a720782cbec2ca6d
rubenorozco <rubendarioorozcozapata@gmail.com>
2018-12-20 10:25:38 -0500

    se copia archivos del build

---
+++ precache-manifest.e13e27c8f0ea992981859d9906f2c301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rubenorozco.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c4746b82028c5951d2b0",
+    "url": "/rubenorozco.github.io/static/js/runtime~main.c4746b82.js"
+  },
+  {
+    "revision": "11f97482a55d823c588f",
+    "url": "/rubenorozco.github.io/static/js/main.11f97482.chunk.js"
+  },
+  {
+    "revision": "590d638ba5d328f0e567",
+    "url": "/rubenorozco.github.io/static/js/1.590d638b.chunk.js"
+  },
+  {
+    "revision": "11f97482a55d823c588f",
+    "url": "/rubenorozco.github.io/static/css/main.448e8aa0.chunk.css"
+  },
+  {
+    "revision": "13bdb352dc04415eb7743544ac4849cb",
+    "url": "/rubenorozco.github.io/index.html"
+  }
+];
